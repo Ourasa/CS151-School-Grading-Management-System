bb4c3ff246c40cacc6a8aa4c1a67b46b448e41fc
Ourasa <111930214+Ourasa@users.noreply.github.com>
2022-11-11 17:01:05 -0800

    basic login screen thingy

--- CS151-Proj/src/project/UserInterface.java
+++ CS151-Proj/src/project/UserInterface.java
@@ -5,7 +5,7 @@ import java.awt.Font;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class UserInterface {
+public class UserInterface implements ActionListener {
 	private Controller control;
 	
 	/*
@@ -23,10 +23,64 @@ public class UserInterface {
 	 * 
 	 */
 	
+	private JFrame frame;
+	private JPanel loginPanel;
+	private JTextField idField;
+	private JPasswordField pwdField;
+	private JButton loginButton;
+	private JLabel statusLabel;
+	
 	
 	public UserInterface(Controller control) {
 		this.control = control;
+		frame = new JFrame();
 		
+		frame.setSize(425, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("Pain System");
+		
+		loginPanel = new JPanel();
+		loginPanel.setLayout(null);
+		
+		idField = new JTextField();
+		idField.setBounds(100, 100, 200, 25);
+		loginPanel.add(idField);
+		
+		pwdField = new JPasswordField();
+		pwdField.setBounds(100, 150, 200, 25);
+		loginPanel.add(pwdField);
+		
+		
+		loginButton = new JButton();
+		loginButton.addActionListener(this);
+		loginButton.setText("Login");
+		loginButton.setBounds(150,200, 100, 25);
+		loginPanel.add(loginButton);
+		
+		statusLabel = new JLabel();
+		statusLabel.setBounds(150, 250, 100, 25);
+		loginPanel.add(statusLabel);
+		
+		frame.add(loginPanel);
+		frame.setVisible(true);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == loginButton) {
+			String pwd = "";
+			for (int i = 0; i < pwdField.getPassword().length; i++) {
+				pwd += pwdField.getPassword()[i];
+			}
+			
+			if (control.loginUser(idField.getText(), pwd)) {
+				statusLabel.setText("Well, it worked!");
+			} else {
+				statusLabel.setText("F for FAILURE");
+			}
+		}
 	}
 
 	
@@ -34,7 +88,7 @@ public class UserInterface {
 	 * List of possible commands it will need:
 	 * 
 	 * - Get the current user
-	 * 
+	 * - Log out the current user
 	 * 
 	 * Admin 
 	 * - Add a new Student, Professor, or Admin
