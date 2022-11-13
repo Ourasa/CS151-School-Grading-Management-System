d7024d7f94eabcd4cfc85110aded78445c671d08
Ourasa <111930214+Ourasa@users.noreply.github.com>
2022-11-12 23:27:20 -0800

    Delete CS151-Proj directory

--- CS151-Proj/src/project/Admin.java
+++
@@ -1,8 +0,0 @@
-package project;
-
-public class Admin extends User {
-
-	public Admin(String firstName, String lastName, String id, String password) {
-		super(firstName, lastName, id, password);
-	}
-}
