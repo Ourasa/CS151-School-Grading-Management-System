7ccfaf964843455111ed54eab6703f04c5e760ff
nir-g0 <ngub24@gmail.com>
2022-11-22 01:49:20 -0800

    Update AdminOptionScroll.java

--- CS151-Proj/src/project/UI/AdminOptionScroll.java
+++ CS151-Proj/src/project/UI/AdminOptionScroll.java
@@ -57,23 +57,26 @@ public class AdminOptionScroll extends JScrollPane implements ActionListener {
 		if (e.getSource() == adminOptionConfirmBtn) {
 
 			if (((String) adminOptionsBox.getSelectedItem()).equals("Add User")) {
+				frame.adminAddUser = new AdminAddUser(frame);
 				frame.pageTransition(frame.adminAddUser);
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove User")) {
+				frame.adminRemoveUser = new AdminRemoveUser(frame);
 				frame.adminRemoveUser.updateAdminRemoveUserScreen();
 				frame.pageTransition(frame.adminRemoveUser);
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Add Course")) {
+				frame.adminAddCourse = new AdminAddCourse(frame);
 				frame.adminAddCourse.updateAdminAddCourseScreen();
 				frame.pageTransition(frame.adminAddCourse);
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Course")) {
+				frame.adminRemoveCourse = new AdminRemoveCourse(frame);
 				frame.adminRemoveCourse.updateAdminRemoveCourseScreen();
 				frame.pageTransition(frame.adminRemoveCourse);
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Set Professor for Course")) {
-				
-				
+
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Professor from Course")) {
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Add Student to Course")) {
@@ -81,15 +84,16 @@ public class AdminOptionScroll extends JScrollPane implements ActionListener {
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Student from Course")) {
 
 			} else if (((String) adminOptionsBox.getSelectedItem()).equals("View All Users")) {
+				frame.adminViewUsers = new AdminViewUser(frame);
 				frame.adminViewUsers.updateViewUsersScreen();
 				frame.pageTransition(frame.adminViewUsers);
 			}
 		} else {
 			frame.adminOptionScroll.adminOptionsBox.setSelectedIndex(0);
-			frame.loginScroll= new LoginScrollPane(frame);
+			frame.loginScroll = new LoginScrollPane(frame);
 			frame.pageTransition(frame.loginScroll);
 			frame.control.logoutUser();
-			//System.out.println("here");
+			// System.out.println("here");
 		}
 	}
 }
