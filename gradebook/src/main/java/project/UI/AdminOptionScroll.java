7823fd69867862fbef03947190143f0da635f386
nir-g0 <ngub24@gmail.com>
2022-11-23 14:21:26 -0800

    searchable combobox attempt1

--- gradebook/src/main/java/project/UI/AdminOptionScroll.java
+++ gradebook/src/main/java/project/UI/AdminOptionScroll.java
@@ -16,6 +16,7 @@ public class AdminOptionScroll extends JScrollPane implements ActionListener {
 	JButton adminOptionConfirmBtn;
 	JButton adminLogoutBtn;
 	JLabel adminWelcomeLabel;
+	AutoComplete autoComplete;
 	UserInterface frame;
 
 	public AdminOptionScroll(UserInterface in) {
@@ -26,9 +27,9 @@ public class AdminOptionScroll extends JScrollPane implements ActionListener {
 		String[] adminOptions = { "Add User", "Remove User", "Add Course", "Remove Course", "Set Professor for Course",
 				"Remove Professor from Course", "Add Student to Course", "Remove Student from Course",
 				"View All Users" };
-		adminOptionsBox = new JComboBox<String>(adminOptions);
-		adminOptionsBox.setBounds(100, 120, 200, 25);
-		this.add(adminOptionsBox);
+		autoComplete = new AutoComplete(adminOptions);
+		autoComplete.setBounds(100, 120, 200, 25);
+		this.add(autoComplete);
 
 		adminOptionConfirmBtn = new JButton();
 		adminOptionConfirmBtn.addActionListener(this);
@@ -54,46 +55,46 @@ public class AdminOptionScroll extends JScrollPane implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == adminOptionConfirmBtn) {
 
-			if (((String) adminOptionsBox.getSelectedItem()).equals("Add User")) {
+			if (((String) autoComplete.getSelectedItem()).equals("Add User")) {
 				frame.adminAddUser = new AdminAddUser(frame);
 				frame.pageTransition(frame.adminAddUser);
 
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove User")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Remove User")) {
 				frame.adminRemoveUser = new AdminRemoveUser(frame);
 				frame.adminRemoveUser.updateAdminRemoveUserScreen();
 				frame.pageTransition(frame.adminRemoveUser);
 
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Add Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Add Course")) {
 				frame.adminAddCourse = new AdminAddCourse(frame);
 				frame.adminAddCourse.updateAdminAddCourseScreen();
 				frame.pageTransition(frame.adminAddCourse);
 
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Remove Course")) {
 				frame.adminRemoveCourse = new AdminRemoveCourse(frame);
 				frame.adminRemoveCourse.updateAdminRemoveCourseScreen();
 				frame.pageTransition(frame.adminRemoveCourse);
 
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Set Professor for Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Set Professor for Course")) {
 				frame.setProfessorToCourse = new SetProfessorToCourse(frame);
 				frame.setProfessorToCourse.courseListBox();
 				frame.pageTransition(frame.setProfessorToCourse);
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Professor from Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Remove Professor from Course")) {
 
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Add Student to Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Add Student to Course")) {
 				frame.addStudentToCourse = new AddStudentToCourse(frame);
 				frame.addStudentToCourse.courseListBox();
 				frame.pageTransition(frame.addStudentToCourse);
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("Remove Student from Course")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("Remove Student from Course")) {
 				frame.removeStudentFromCourse = new RemoveStudentFromCourse(frame);
 				frame.removeStudentFromCourse.courseListBox();
 				frame.pageTransition(frame.removeStudentFromCourse);
-			} else if (((String) adminOptionsBox.getSelectedItem()).equals("View All Users")) {
+			} else if (((String) autoComplete.getSelectedItem()).equals("View All Users")) {
 				frame.adminViewUsers = new AdminViewUser(frame);
 				frame.adminViewUsers.updateViewUsersScreen();
 				frame.pageTransition(frame.adminViewUsers);
 			}
 		} else {
-			frame.adminOptionScroll.adminOptionsBox.setSelectedIndex(0);
+			frame.adminOptionScroll.autoComplete.setSelectedIndex(0);
 			frame.loginScroll = new LoginScrollPane(frame);
 			frame.pageTransition(frame.loginScroll);
 			frame.control.logoutUser();
