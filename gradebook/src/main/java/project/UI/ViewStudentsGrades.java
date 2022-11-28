64c9123c8dfb1cfadb2d533509bf192dcfec5cec
nir-g0 <ngub24@gmail.com>
2022-11-28 01:58:31 -0800

    Minor Fixes

--- gradebook/src/main/java/project/UI/ViewStudentsGrades.java
+++ gradebook/src/main/java/project/UI/ViewStudentsGrades.java
@@ -39,7 +39,7 @@ public class ViewStudentsGrades extends JScrollPane implements ActionListener {
 	public ViewStudentsGrades(UserInterface in) {
 		frame = in;
 		this.setLayout(null);
-		this.setPreferredSize(new Dimension(500, 1000));
+		this.setPreferredSize(new Dimension(1000, 500));
 		tempComponents = new ArrayList<>();
 		addComponents();
 
